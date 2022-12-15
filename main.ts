99af018688f01bc192fae6b1034d303ce4dc956a
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2022-12-15 11:28:34 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+
+let pocet6
+let pocethodu = 0
+
+input.onGesture(Gesture.Shake, function() {
+    let nahodnecislo = randint(1, 6) 
+    basic.showNumber(nahodnecislo)
+    let pocethodu = ++1
+    }   
+    
+    if (nahodnecislo = (1, 5)) {
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    }
+   
+    if (nahodnecislo == 6) {
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    
+    if (pocet6 = 3){
+        basic.showNumber (pocethodu)
+        music.playTone(Note.G3, music.beat(BeatFraction.Whole))
+    }
+
+    }
